a0bc12b463df7c93bdff9f7d1dd2c464ca73f69e
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2024-03-09 21:19:57 +0300

    top-level

--- GeezDemo/Program.cs
+++ GeezDemo/Program.cs
@@ -1,23 +1,17 @@
 ﻿using System.Text;
 using GeezNet;
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        var geez = Geez.Create();
-        Console.WriteLine(geez.ToGeez(123));              // ፻፳፫
-        Console.WriteLine(geez.ToGeez(1234));             // ፲፪፻፴፬
-        Console.WriteLine(geez.ToGeez(1986));             // ፲፱፻፹፮
-        Console.WriteLine(geez.ToGeez(1000000));          // ፻፼
+Console.OutputEncoding = Encoding.UTF8;
+var geez = Geez.Create();
+Console.WriteLine(geez.ToGeez(123));              // ፻፳፫
+Console.WriteLine(geez.ToGeez(1234));             // ፲፪፻፴፬
+Console.WriteLine(geez.ToGeez(1986));             // ፲፱፻፹፮
+Console.WriteLine(geez.ToGeez(1000000));          // ፻፼
 
-        // or you can even do the reverse
-        // this is the tricky part you wouldn't see else where
-        // at least for now
+// or you can even do the reverse
+// this is the tricky part you wouldn't see else where
+// at least for now
 
-        Console.WriteLine(geez.ToAscii("፻፳፫"));           // 123
-        Console.WriteLine(geez.ToAscii("፲፪፻፴፬"));         // 1234
-        Console.WriteLine(geez.ToAscii("፲፱፻፹፮"));        // 1986
-        Console.WriteLine(geez.ToAscii("፻፼"));
-    }
-}
+Console.WriteLine(geez.ToAscii("፻፳፫"));           // 123
+Console.WriteLine(geez.ToAscii("፲፪፻፴፬"));         // 1234
+Console.WriteLine(geez.ToAscii("፲፱፻፹፮"));        // 1986
+Console.WriteLine(geez.ToAscii("፻፼"));
